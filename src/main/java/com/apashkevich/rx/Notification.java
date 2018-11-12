2cbf124f5910f99a1027d3020b8352979701bfdb
Andrei Pashkevich <Andrei_Pashkevich2@epam.com>
2018-11-12 12:11:17 +0300

    sdwdwd

---
+++ src/main/java/com/apashkevich/rx/Notification.java
@@ -0,0 +1,38 @@
+package com.apashkevich.rx;
+
+public class Notification {
+
+    private int id;
+    private String name;
+    private String message;
+
+    public Notification(int id, String name, String message) {
+        this.id = id;
+        this.name = name;
+        this.message = message;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/apashkevich/rx/RestTest.java
@@ -0,0 +1,8 @@
+package com.apashkevich.rx;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestTest extends Application {
+}
